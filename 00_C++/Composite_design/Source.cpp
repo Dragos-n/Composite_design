d9a524e996c4a95084318267515dc29e1701dd0c
uib17221 <dragos.marian.nicolae-ext@continental-corporation.com>
2021-02-13 10:06:44 +0200

    Final print

--- 00_C++/Composite_design/Source.cpp
+++ 00_C++/Composite_design/Source.cpp
@@ -1,80 +1,84 @@
-// Composite design.cpp : Defines the entry point for the console application.
-//
+/* Composite design.cpp */
 
 #include <vector>
 #include <string>
 #include <iostream>
 using namespace std;
 
+/* Component class */
+
 class Menu
 {
 public:
 
 	virtual void Add(Menu* new_menu) {};
-
-	virtual ~Menu() {};
 	virtual void Component_print() {};
-
+	virtual ~Menu() {};
 };
 
-class Leaf : public Menu {
+/*Leaf class*/
+
+class SubMenu : public Menu {
 
 private:
 
-	string leaf;
+	string submenu_s;
 
 public:
 
-	Leaf(string leaf)
+	SubMenu(string submenu_s)
 	{
-		this->leaf = leaf;
+		this->submenu_s = submenu_s;
 	};
 
-	~Leaf()
+	~SubMenu()
 	{
-		std::cout << leaf << " deleted" << endl;
+		submenu_s.erase(std::remove(submenu_s.begin(), submenu_s.end(), '\t'), submenu_s.end());
+		std::cout << submenu_s << " deleted" << endl;
 	};
 
-	void Add(Menu* new_submenu)
+	virtual void Add(Menu* new_submenu_p)
 	{
-		// this->leaf = new_submenu;
 	};
 
 	void Component_print()
 	{
-		cout << "\t" << this->leaf << " ";
+		cout << this->submenu_s << endl;
 	};
 };
+
+/*Composite class*/
 
 class Composite : public Menu {
 
 private:
-	vector <Menu*> gList;
-	string menu;
+	vector <Menu*> menu_list;
+	string menu_s;
 
 public:
 
-	void add(Menu* aComposite)
+	void Add(Menu* composite_p)
 	{
-		gList.push_back(aComposite);
+		menu_list.push_back(composite_p);
 	}
 
-	Composite(string new_menu)
+	Composite(string new_menu_s)
 	{
-		this->menu = new_menu;
+		this->menu_s = new_menu_s;
 	};
 
 	~Composite()
 	{
-		std::cout << menu << " deleted" << endl;
+		menu_s.erase(std::remove(menu_s.begin(), menu_s.end(), '\t'), menu_s.end());
+		std::cout << menu_s << " deleted" << endl;
 	};
 
 	void Component_print()
 	{
-		cout << "\t" << this->menu << "-" << endl;
-		for (auto leaf : gList)
+		cout << this->menu_s << endl;
+		for (auto submenu : menu_list)
 		{
-			leaf->Component_print();
+			submenu->Component_print();
 		}
 		cout << endl;
 	};
@@ -83,20 +87,28 @@ public:
 
 int main()
 {
-	Composite c1("Menu1");
-	Composite c2("Menu2");
-	Composite c3("Menu3");
-	Leaf l1("leaf1");
-	Leaf l2("leaf2");
-	Leaf l3("leaf3");
-	c1.add(&l1);
-	c1.add(&l2);
-	c2.add(&l1);
-	c2.add(&l3);
-	c3.add(&c1);
-	c3.add(&c2);
-	c1.Component_print();
-	c2.Component_print();
-	c3.Component_print();
+	Composite m1("Menu1");
+	Composite m2("Menu2");
+	Composite m3("Menu3");
+	SubMenu s1("\tSubMenu1");
+	SubMenu s2("\tSubMenu2");
+	Composite s3("\tSubMenu3");
+	SubMenu ss1("\t\tSubSubMenu1");
+	SubMenu ss2("\t\tSubSubMenu2");
+	
+	m1.Add(&s1);
+	m1.Add(&s2);
+
+	s3.Add(&ss1);
+	s3.Add(&ss2);
+	m2.Add(&s1);
+	m2.Add(&s3);
+	
+	m3.Add(&m1);
+	m3.Add(&m2);
+
+	m1.Component_print();
+	m2.Component_print();
+	m3.Component_print();
 	return 0;
 }
